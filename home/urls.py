bcbd3f1d5c12ca44322e492a39250dd391ffb5f4
Jahongir <jahongirq702@gmail.com>
2024-04-27 21:22:16 +0500

    qwerty

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from home.views import get_info
+from home.views import get_info_1
 
 urlpatterns = [
-    path('app_url', get_info, name='app_url')
+    path('app_url', get_info, name='app_url'),
+    path('app_url_1', get_info_1, name='app_url_1')
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 def get_info(request):
     return HttpResponse("Hello Joxon")
 
+
+def get_info_1(request):
+    return HttpResponse("Whats up")
+
 # Create your views here.
